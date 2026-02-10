b2cb8c31214326349fde8c59ecc67609e1e6cc6e
Maxim Shevchenko <tier4ik@gmail.com>
2026-02-10 11:05:15 +0300

    Changed public path for static files

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     outDir: 'docs',
-    emptyOutDir: true,
+    emptyOutDir: true
   },
+  base: '/weather-app/'
 });
